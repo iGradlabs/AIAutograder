17817b91d8997912b3461acf8d89e09ca9f75ee3
SHYAM T.R <t.r.shyam0007@gmail.com>
2023-09-20 20:42:06 +0530

    added signin templates & auth.py email_ver

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 
-# flask app name shyam
+# flask app name
 app = Flask(__name__)
 
 
@@ -8,6 +8,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/login')
+def loig_in():
+    return render_template('auth-login-basic.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
