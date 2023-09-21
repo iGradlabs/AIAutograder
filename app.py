6d3c7a6a51904d0ea4cc66191516d90a1eca49b1
KISHORMV <kishormuthu2@gmail.com>
2023-09-21 11:00:39 +0530

    made #hello

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 
+#hello
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', data=data)
-
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+     app.run(debug=True)
+    
+
