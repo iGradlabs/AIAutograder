8fe4acd593552b5e67349f339fb6e2244864ceea
Devendhiran <devsavi07@gmail.com>
2023-09-21 19:57:19 +0530

    modified

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 
+# flask app name
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
-    return render_template('index.html', data=data)
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-# demo line addedff
-#hello
