7082082a002a056f69e48a24045938c45c139671
SHYAM T.R <t.r.shyam0007@gmail.com>
2023-09-24 17:05:32 +0530

    details_sending_step

--- app.py
+++ app.py
@@ -1,4 +1,22 @@
 from flask import Flask, render_template, request, redirect, url_for, session
+import email_send
+
+# #<<<--- firebase config ---->>>
+# config={
+#   'apiKey': "AIzaSyCJW1arUPVjgJRxLYfhX9vbztByaUytAOM",
+#   'authDomain': "agt1-16b90.firebaseapp.com",
+#   'projectId': "agt1-16b90",
+#   'storageBucket': "agt1-16b90.appspot.com",
+#   'messagingSenderId': "356668542049",
+#   'appId': "1:356668542049:web:a57f7c56467f8bef51ee20",
+#   'measurementId': "G-EXR7R90QRQ",
+#   'databaseURL':" https://agt1-16b90-default-rtdb.asia-southeast1.firebasedatabase.app/"
+#   }
+# firebase = pyrebase.initialize_app(config)  
+# db=firebase.database()
+# auth=firebase.auth()
+# #<<<--- firebase config ---->>>
+
 
 # flask app name
 app = Flask(__name__)
@@ -8,9 +26,50 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/login')
-def loig_in():
-    return render_template('auth-login-basic.html')
+
+@app.route('/auth-register-basic.html',methods=['POST','GET'])
+def sign_up():
+
+    if request.method == 'POST':
+        username=request.form['username']
+        email=request.form['email']
+        password=request.form['password']
+
+        user_data = {
+        "username": username,
+        "email": email,
+        "password": password
+        }
+    
+        email_send.send_mail(user_data)
+
+    return render_template('auth-register-basic.html')
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+    # user_data={}
+    # company_users=None
+    # def user_sign_up(data):
+    #     company_users=None
+    #     try:
+    #         db.child("company_users").child(data["username"]).set(data)
+          
+    #         # company_users=data["email"]
+    #         company_users=auth.create_user_with_email_and_password(data["email"],'password')
+    #     except:
+    #         print('User already exist')
+    #     return company_users
+
+    #   user_sign_up(user_data)
---
+++ email_send.py
@@ -0,0 +1,72 @@
+import smtplib
+from email.mime.text import MIMEText
+import pyrebase
+config={
+  'apiKey': "AIzaSyCJW1arUPVjgJRxLYfhX9vbztByaUytAOM",
+  'authDomain': "agt1-16b90.firebaseapp.com",
+  'projectId': "agt1-16b90",
+  'storageBucket': "agt1-16b90.appspot.com",
+  'messagingSenderId': "356668542049",
+  'appId': "1:356668542049:web:a57f7c56467f8bef51ee20",
+  'measurementId': "G-EXR7R90QRQ",
+  'databaseURL':" https://agt1-16b90-default-rtdb.asia-southeast1.firebasedatabase.app/"
+  }
+
+firebase = pyrebase.initialize_app(config)  
+db=firebase.database()
+auth=firebase.auth()
+
+def create_db(data):
+    # db.child("company_users").child(data["username"]).set(data)
+
+    users=db.child("company_users")#LIST OF USERS ADDED IN THE REALTIME DATABASE
+    users_data = users.get().val()
+    # print(users_data)
+
+    if users_data is not None and data["username"] in users_data:
+        print("Username exists.")
+    else:
+        db.child("company_users").child(data["username"]).set(data)
+        print("Username does not exist.")
+
+
+def create_user_id(user_data):
+        try:
+            user = auth.create_user_with_email_and_password(user_data["email"],user_data["password"])
+            print("User created successfully.")
+            return user
+        except Exception as e:
+            print("Error creating user:", str(e))
+            return None
+
+
+def send_mail(data):
+
+    # Set up the email server
+    smtp_server = "smtp.gmail.com"
+    smtp_port = 587
+
+    # Set up the email message
+    sender_email = "t.r.shyam0007@gmail.com"#consided as webpage mail
+    receiver_email = "ktraveendran25@gmail.com"#consided as rep
+    subject = "New user sign-up request"
+    message =f"Username: {data['username']}\nEmail: {data['email']}"
+
+
+    #<<<----- Set up the MIME message ---->>>
+    msg = MIMEText(message)
+    msg["Subject"] = subject
+    msg["From"] = sender_email
+    msg["To"] = receiver_email
+    #<<<----- Set up the MIME message ---->>>
+
+
+    # Send the email
+    server = smtplib.SMTP(smtp_server, smtp_port)
+    server.starttls()
+    server.login(sender_email, "fvam btzk exbf ivxz")
+    server.sendmail(sender_email, receiver_email, msg.as_string())
+    server.quit()
+    # Send the email
+
+
