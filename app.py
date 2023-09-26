8709591f8f6de3fcb39b430334d321cb46391749
Girija gayathri M <girijagayu2003@gmail.com>
2023-09-26 16:02:10 +0530

    checking

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, request, redirect, url_for, session
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/index')
+
 def index():
     return render_template('index.html')
 
