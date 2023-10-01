5536e0aebe5d7994d91da47a1a2b0610b6d1a0e0
KISHORMV <kishormuthu2@gmail.com>
2023-10-01 20:35:51 +0530

    created a button "create question' in index.html linked 2 html files with flask

--- app.py
+++ app.py
@@ -8,6 +8,11 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/create_question')
+
+def second():
+    return render_template('sample_form.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
