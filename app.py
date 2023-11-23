25b6ea94f1435985a44a87e96b51121642d99e74
Yogesh M V <yogeshmv744@gmail.com>
2023-11-23 14:54:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,30 @@
-from flask import Flask, render_template, request,jsonify
-
+from flask import Flask, render_template, request
+import randomizer1
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('form.html')
+    return render_template('index.html')
 
-@app.route('/submit', methods=['POST'])
-def submit():
-    total_marks = request.form['totalMarks']
-    mcq_percentage = request.form['mcqQuestionsPercentage']
-    coding_percentage = request.form['codingQuestionsPercentage']
-    topics = request.form.getlist('topics')
-    difficulty_level = request.form['difficultyLevel']
+@app.route('/input_form', methods=['GET', 'POST'])
+def input_form():
+    if request.method == 'POST':
+        language = request.form['language']
+        percentage_coding = request.form['percentage_coding']
+        tags = request.form.getlist('tags')
+        total_marks = request.form['total_marks']
+        difficulty_level = request.form['difficulty_level']
+        # print(total_marks)
+        # print(type(total_marks))
+        total_marks=int(total_marks)
+        percentage_coding=int(percentage_coding)
+        print(type(total_marks))
 
+        randomizer1.main(language, percentage_coding, tags, total_marks, difficulty_level)
+        return 'Form submitted successfully!'
+    return render_template('main.html',total_marks=total_marks)
 
-    info_json = {
-        "Total Marks": total_marks,
-        "MCQ Questions Percentage": mcq_percentage,
-        "Coding Questions Percentage": coding_percentage,
-        "Topics": topics,
-        "Difficulty Level": difficulty_level
-    }
-
-    return jsonify(info_json)
+    # return render_template('main.html', language=language, percentage_coding=percentage_coding, tags=tags, total_marks=total_marks, difficulty_level=difficulty_level)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
