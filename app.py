525cd3e4973135a98c075ffe36c79c6a3eb88642
Yogesh M V <yogeshmv744@gmail.com>
2023-11-23 15:09:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('form.html')
 
 @app.route('/input_form', methods=['GET', 'POST'])
 def input_form():
